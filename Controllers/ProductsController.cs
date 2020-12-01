71a574f241a4d94d17376e0cca21cfdbdc4ac21d
jorgemarcoux <jorgemarcoux@gmail.com>
2020-12-01 11:41:34 -0500

    Controllers created

---
+++ Controllers/ProductsController.cs
@@ -0,0 +1,21 @@
+using Microsoft.AspNetCore.Mvc;
+using System.Text.Encodings.Web;
+
+namespace Rocket_Elevators_Customer_Portal.Controllers
+{
+
+    public class InterventionFormController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Welcome()
+        {
+            ViewData["Message"] = "Your welcome message";
+
+            return View();
+        }
+    }
+}
